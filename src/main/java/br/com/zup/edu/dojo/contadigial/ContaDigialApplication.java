f908e07fad9d14aaa45a3058bb7bfe047ef1b01b
alexandre-zup <alexandre.santos@zup.com.br>
2021-10-04 16:18:30 -0300

    setup

---
+++ src/main/java/br/com/zup/edu/dojo/contadigial/ContaDigialApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.dojo.contadigial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaDigialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaDigialApplication.class, args);
+	}
+
+}
