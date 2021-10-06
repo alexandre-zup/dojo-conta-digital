e461596538cc7f47d7d2355c0a7801023334cef1
osmar-jr <osmar.junior@zup.com.br>
2021-10-06 14:28:45 -0300

    Adicionar Logger nas operações de Credito e Debito

--- src/main/java/br/com/zup/edu/dojo/contadigital/transacao/Debito.java
+++ src/main/java/br/com/zup/edu/dojo/contadigital/transacao/Debito.java
@@ -1,13 +1,18 @@
 package br.com.zup.edu.dojo.contadigital.transacao;
 
 import br.com.zup.edu.dojo.contadigital.conta.Conta;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.math.BigDecimal;
 
 public class Debito implements Transacao {
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass().getSimpleName());
+
     @Override
     public void executa(Conta conta, BigDecimal valor) {
+        logger.info("Nova transação de Débito realizada.");
         conta.debita(valor);
     }
 }
